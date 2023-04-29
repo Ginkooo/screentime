92ed5f7ab56e439168649ca17ba3ba2f0e945317
Piotr Czajka <czajka@protonmail.com>
2023-04-29 20:49:31 +0200

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use axum::routing::get;
 
 use std::path::PathBuf;
 
+
 use mockall::predicate::*;
-use mockall::*;
 use std::time::Duration;
 
 use active_win_pos_rs::get_active_window;
@@ -25,18 +25,16 @@ fn seconds_to_hms(total_seconds: u32) -> String {
 
 type ScreenTime = HashMap<String, u32>;
 
+pub struct Config {}
+
 #[automock]
-trait ConfigGetter {
-    fn get_config_file_path() -> PathBuf {
-        let mut config_file = dirs::config_dir().unwrap();
+impl Config {
+    pub fn get_screentime_file_path() -> PathBuf {
+        let mut config_file = dirs::data_local_dir().unwrap();
         config_file.push("screentime.db");
         config_file
     }
 }
-
-struct Config {}
-
-impl ConfigGetter for Config {}
 
 fn get_today_as_str() -> String {
     let dt = Local::now();
@@ -51,13 +49,14 @@ fn update_screentime() {
         "unknown".to_string()
     };
 
-    let config_file = Config::get_config_file_path();
+    let config_file = Config::get_screentime_file_path();
 
     let db = DB::open(config_file).unwrap();
 
     {
         let tx = db.tx(true).unwrap();
         tx.get_or_create_bucket("screentime").unwrap();
+        tx.commit().unwrap();
     }
 
     let dt = get_today_as_str();
@@ -89,7 +88,7 @@ async fn run_usage_time_updater() {
 }
 
 async fn get_inlinehms() -> String {
-    let config_file = Config::get_config_file_path();
+    let config_file = Config::get_screentime_file_path();
     let dt = get_today_as_str();
     let db = DB::open(config_file).unwrap();
     let tx = db.tx(false).unwrap();
@@ -123,14 +122,40 @@ fn build_router() -> Router {
 async fn main() {
     tokio::task::spawn(run_usage_time_updater());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8465").await.unwrap();
     axum::serve(listener, build_router()).await.unwrap();
 }
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
+    use anyhow::Result;
+    
+    
+    use rstest::fixture;
+    use rstest::rstest;
+    use tempfile::{TempDir};
+
+    use crate::MockConfig;
+
+    #[fixture]
+    fn screentime_path() -> Result<PathBuf> {
+        let tempdir = TempDir::new()?;
+        let mut path = tempdir.into_path();
+        path.push("screentime");
+        Ok(path)
+    }
+
+    #[fixture]
+    fn mocked_config(_screentime_path: Result<PathBuf>) -> Result<()> {
+        let _config = MockConfig::new();
+        todo!();
+    }
+
+    #[rstest]
     #[tokio::test]
-    async fn test_it_works() {
+    async fn test_it_works(_screentime_path: Result<PathBuf>, _mocked_config: Result<()>) {
         use axum_test_helper::TestClient;
 
         let client = TestClient::new(crate::build_router());
